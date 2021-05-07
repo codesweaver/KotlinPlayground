54a6417724c10f942a8c04a2f615c4f243e8f2d6
codesweaver <codetrain999@gmail.com>
2021-05-07 11:34:14 +0900

    Kotlin Functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,22 @@
+fun printMessage(message: String): Unit {
+    println(message);
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message");
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y;
+}
+
+fun multiply(x: Int, y: Int) = x * y
+
 fun main() {
-    println("What's your name?")
-    val name = readLine();
-    println("Hello $name")
+    printMessage("Hello");
+    printMessageWithPrefix("Hello", "Log");
+    printMessageWithPrefix("Hello");
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+    println(sum(3, 2))
+    println(multiply(2, 4))
 }
