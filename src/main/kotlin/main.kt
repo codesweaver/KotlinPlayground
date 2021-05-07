40d46f5d8349fa32e2bdcfb1be68261a8b3d0d98
codesweaver <codetrain999@gmail.com>
2021-05-07 13:47:58 +0900

    infix 함수와 pair.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,24 @@
-fun printMessage(message: String): Unit {
-    println(message);
-}
-
-fun printMessageWithPrefix(message: String, prefix: String = "Info") {
-    println("[$prefix] $message");
-}
-
-fun sum(x: Int, y: Int): Int {
-    return x + y;
-}
-
-fun multiply(x: Int, y: Int) = x * y
-
 fun main() {
-    printMessage("Hello");
-    printMessageWithPrefix("Hello", "Log");
-    printMessageWithPrefix("Hello");
-    printMessageWithPrefix(prefix = "Log", message = "Hello")
-    println(sum(3, 2))
-    println(multiply(2, 4))
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "Bye ")
+
+    val pair =  "ferrari" to "Katrina"
+    println(pair);
+
+    infix fun String.onto(other: String) = Pair(this, other)
+    val myPair = "McLaren" onto "Lucas"
+    println(myPair)
+
+    val sophia = Person("Sophia")
+//    println(sophia.name)
+//    sophia.name = "codesweaver"
+    val claudia = Person("Claudia")
+    sophia likes claudia
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) {
+        likedPeople.add(other)
+    }
 }
