0376b4e58370a5918872f7b5d0ad846b57f2c83b
codesweaver <codetrain999@gmail.com>
2021-05-07 16:13:08 +0900

    Null Safety

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,27 @@
 fun main() {
-    var a: String
-    val b: Int
+    var neverNull: String = "This can't be null"
+    // neverNull = null;
 
-    // println(a)
-    b = 3
-    println(b)
+    var nullable: String?  = "You can keep a null here"
+    nullable = null;
+
+    var inferredNonNull = "the compiler assumes non-null"
+    // inferredNonNull = null
+
+    println(strLength(neverNull))
+    // strLength(nullable) // Type mismatch
+
+    println(strLengthWithNull(nullable))
+}
+
+fun strLength(notNull: String): Int {
+    return notNull.length
+}
+
+fun strLengthWithNull(maybeString: String?): String {
+    if (maybeString != null && maybeString.length > 0) {
+        return "String of length ${maybeString.length}"
+    } else {
+        return "empty or null string"
+    }
 }
