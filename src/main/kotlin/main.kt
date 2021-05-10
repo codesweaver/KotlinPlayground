ff9bab23256c9b98235cfe6c7e1a0234f19829bc
codesweaver <codetrain999@gmail.com>
2021-05-10 10:57:27 +0900

    Inheritance 1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,16 @@
-class MutableStack<E>(vararg items: E) {
-    private val elements = items.toMutableList()
-
-    fun push(element: E) = elements.add(element)
-
-    fun peek(): E = elements.last()
-
-    fun pop(): E = elements.removeAt(elements.size - 1)
-
-    fun isEmpty() = elements.isEmpty()
-
-    fun size() = elements.size
-
-    override fun toString() = "MutableStack(${elements.joinToString()})"
+open class Dog {
+    open fun sayHello() {
+        println("bow wow")
+    }
 }
 
-fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements)
+class Yorkshire : Dog() {
+    override fun sayHello() {
+        println("wif wif!")
+    }
+}
 
 fun main() {
-    val stack = mutableStackOf(0.62, 3.14, 2.7)
-    println(stack)
-
-    val anotherStack = mutableStackOf(3, 4, 5, 6)
-    println(anotherStack)
+    val dog: Dog = Yorkshire()
+    dog.sayHello()
 }
